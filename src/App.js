ecabb80b3aa34f1eaf569e0f96b0f26617d0855d
Manoj Borad <105836848+manojb-ctl@users.noreply.github.com>
2022-06-06 18:49:23 +0530

    Add files via upload
    Demo two done

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import React from "react";
+// import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: "New Desk Paper",
+      amount: 94.12,
+      date: new Date(2020, 1, 14),
+    },
+    {
+      id: 2,
+      title: "Car Insurance",
+      amount: 923.5,
+      date: new Date(2021, 5, 24),
+    },
+    {
+      id: 3,
+      title: "New TV",
+      amount: 150.66,
+      date: new Date(2021, 7, 9),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>App js File (demo two)</h2>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
